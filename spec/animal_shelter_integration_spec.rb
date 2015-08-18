17391b93f520653649b556cd493c479d7dc65adc
Corrie Smith & Shannon Mulloy <student@epicodus.com>
2015-08-18 15:57:59 -0700

    pass spec to add new applicant

--- spec/animal_shelter_integration_spec.rb
+++ spec/animal_shelter_integration_spec.rb
@@ -15,3 +15,15 @@ describe('add a new animal to the system', {:type => :feature}) do
     expect(page).to have_content('Max')
   end
 end
+
+describe('add a new applicant to the system', {:type => :feature}) do
+  it('adds a new applicant to the system') do
+    visit('/potential_pet_owners')
+    fill_in('name', :with => 'Steve')
+    fill_in('phone', :with => '555-5555')
+    fill_in('animal_species_preference', :with => 'Dog')
+    fill_in('animal_breed_preference', :with => 'German Shepherd')
+    click_button('Add Applicant')
+    expect(page).to have_content('Steve')
+  end
+end
